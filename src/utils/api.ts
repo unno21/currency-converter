ed572d04690cb393f33374470dce17d65e3ef201
Richard Roncales <chadxxx21@gmail.com>
2022-06-30 07:40:54 +0800

    Add util for exchange rate api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,39 +1,13 @@
-// https://exchangeratesapi.io/
-const BASE_URL = 'http://api.exchangeratesapi.io/v1';
-const API_KEY = 'REPLACE_WITH_YOUR_API_KEY'
-
-// TODO: what is the response type in the Promise? We should avoid using 'any'
 type API = (params: {
+  baseUrl: string,
   endpoint: string,
-  params: {
-    base?: string,
-  },
+  params?: {},
+  headers?: {},
 }) => Promise<any>;
 
-const api: API = ({ endpoint, params = {} }) => {
+export const api: API = ({ baseUrl, endpoint, params = {}, headers = {} }) => {
   const searchParams = new URLSearchParams(params);
-  searchParams.append('access_key', API_KEY);
   const queryString = searchParams.toString();
 
-  return fetch(`${BASE_URL}${endpoint}?${queryString}`);
-};
-
-export const fetchRates = async (baseCurrency: string) => {
-  try {
-    const response = await api({ endpoint: '/latest', params: { base: baseCurrency } });
-    const responseText = await response.text();
-    const { rates, error } = JSON.parse(responseText);
-
-    if (error) {
-      throw new Error(error);
-    }
-
-    if (!rates || !Object.keys(rates).length) {
-      throw new Error('Could not fetch rates.');
-    }
-
-    return rates;
-  } catch (errorResponse) {
-    throw errorResponse;
-  }
+  return fetch(`${baseUrl}${endpoint}?${queryString}`, { headers });
 };
